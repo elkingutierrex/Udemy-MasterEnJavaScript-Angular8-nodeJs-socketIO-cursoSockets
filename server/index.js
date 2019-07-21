fa4104137023e01cb3e3adcc34e8a387369810fb
elkingutierrex <elkin.gutierrex@gmail.com>
2019-07-21 06:23:02 -0500

    275 - Conexión al socket

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-var io = require('socket.io');
+var io = require('socket.io')(server);
+
+
+app.use(express.static('client'))
 
 app.get('/hola-mundo', function(req, res){
     res.status(200).send('Hola mundo desde una ruta')
-})
+});
+
+io.on('connection', function(socket){
+    console.log("el nodo con IP: " + socket.handshake.address + " se ha connectado...");
+});
+
 server.listen(6677, function(){
     console.log('Servidor esta funcionando en http://localhost:6677')
 });
