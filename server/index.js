9181adffd5e70d35009e53ad0c09311297b0a713
elkingutierrex <elkin.gutierrex@gmail.com>
2019-07-21 07:12:58 -0500

    276 - Mensajes en el chat

--- server/index.js
+++ server/index.js
@@ -10,8 +10,17 @@ app.get('/hola-mundo', function(req, res){
     res.status(200).send('Hola mundo desde una ruta')
 });
 
+var messages =[{
+    id:1,
+    text:"Bienvenido al chat privado de Socket.io y nodeJS de elkingutierrex...",
+    nickname:"Bot - elkingutierrex"
+
+}]
+
 io.on('connection', function(socket){
     console.log("el nodo con IP: " + socket.handshake.address + " se ha connectado...");
+
+    socket.emit("messages", messages);
 });
 
 server.listen(6677, function(){
